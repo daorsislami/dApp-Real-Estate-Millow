8bd35a606dd92a1be9d4ba96286cd4178abe827e
Daors <daorsislami@gmail.com>
2023-01-09 17:01:36 +0100

    Setup the project. Added the mint and totalSupply function inside RealEstate smart contract. Added variables for nftAddress, seller,buyer, lender, inspector and initialized them inside Escrow constructor

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,21 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public nftAddress; // we need this variable because we wanna store the smart contract address for the NFT for this particular real estate transaction
 
+    address payable public seller; // we add 'payable' because seller will be able to receive crypto currency in this transaction. We'll transfer Ether to them.
+    address public inspector;
+    address public lender; // address is a data type
+
+    constructor(
+        address _nftAddress, 
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
 }
--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,36 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+// importing third-party library for creating the NFT
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract RealEstate is ERC721URIStorage {
+    
+    // this is gonna help us create an ienumerable ERC 721 token so we can create them from scratch
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    constructor() ERC721("Real Estate", "Real") { }
+
+    // create NFT from scratch
+    function mint(string memory tokenURI) public returns (uint256) {
+
+        // here we're gonna let somebody MINT with a specific tokenURI so this is going to be the actual
+        // metadata
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    // this is used to see how many NFTS have been currently minted
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
 }
